f98b53486fdcf1456ad290a2534b39469b9b04d4
annalhq <155549764+annalhq@users.noreply.github.com>
2024-07-30 02:25:09 +0530

    added footer to home

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Hero from "./Hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero/>
+        <Footer/>
       </div>
     </main>
   );
