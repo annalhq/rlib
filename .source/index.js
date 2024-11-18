b33e4b43804a70a0002c2098c71fca2fd3526d64
annalhq <155549764+annalhq@users.noreply.github.com>
2024-11-19 00:31:30 +0530

    added content

--- .source/index.js
+++ .source/index.js
@@ -1,5 +1,7 @@
 import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
 import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=4f3bc6e48351eef47611191451d86bf2ebc0c4238119c3a0c8870a549ef72ca3"
 import * as docs_1 from "../content/docs/test.mdx?collection=docs&hash=4f3bc6e48351eef47611191451d86bf2ebc0c4238119c3a0c8870a549ef72ca3"
-export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"D:/webd/rlib/content/docs/index.mdx"}), toRuntime("doc", docs_1, {"path":"test.mdx","absolutePath":"D:/webd/rlib/content/docs/test.mdx"})];
-export const meta = [];
+import * as docs_2 from "../content/docs/sem1/calculus.mdx?collection=docs&hash=4f3bc6e48351eef47611191451d86bf2ebc0c4238119c3a0c8870a549ef72ca3"
+import * as meta_0 from "../content/docs/sem1/meta.json?collection=meta&hash=4f3bc6e48351eef47611191451d86bf2ebc0c4238119c3a0c8870a549ef72ca3"
+export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"D:/webd/rlib/content/docs/index.mdx"}), toRuntime("doc", docs_1, {"path":"test.mdx","absolutePath":"D:/webd/rlib/content/docs/test.mdx"}), toRuntime("doc", docs_2, {"path":"sem1\\calculus.mdx","absolutePath":"D:/webd/rlib/content/docs/sem1/calculus.mdx"})];
+export const meta = [toRuntime("meta", meta_0, {"path":"sem1\\meta.json","absolutePath":"D:/webd/rlib/content/docs/sem1/meta.json"})];
