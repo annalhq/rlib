f7141b75067f9b4a0071c7b67f0b9d46075fd31e
annalhq <155549764+annalhq@users.noreply.github.com>
2024-11-20 17:51:35 +0530

    added boilerplate mdx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { Banner } from "fumadocs-ui/components/banner";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -11,6 +12,9 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Banner changeLayout={false} id='version'>
+          RLiB is under work! New UI coming soon!
+        </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
@@ -9,5 +9,12 @@ export default {
     './mdx-components.{ts,tsx}',
     './node_modules/fumadocs-ui/dist/**/*.js',
   ],
-  presets: [createPreset()],
+  presets: [
+    createPreset({
+      preset: 'vitepress',
+    }),
+  ],
 };
+
+export default config;
+  
